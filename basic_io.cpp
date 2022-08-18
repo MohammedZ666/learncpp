c6a90c1607afe175d31297ee42eb5017d4664e4f
MohammedZ666 <abuobaidazishanegmail.com>
2022-08-18 11:15:14 +0600

    inital commit

--- basic_io.cpp
+++ basic_io.cpp
@@ -1,44 +1,9 @@
 #include <stdio.h>
-#include <iostream> // this is for cout, cin
-#include <string>   // this is for string
+
 using namespace std;
 
 int main()
 {
-    // === input ===
-    // take a integer
-    int a;
-    cout << "Integer dao \n";
-    cin >> a;
-
-    // take a double
-    double b;
-    cout << "Double dao \n";
-    cin >> b;
-
-    // take a string
-    string str;
-    cout << "String dao \n";
-    cin >> str;
-
-    cout << "\n\n\n";
-
-    // == output ==
-    // print int
-    cout
-        << "Integer ->" << a << "\n";
-
-    // pring double
-    cout
-        << "Double ->" << b << "\n";
-
-    // print string
-    cout << "String ->" << str << "\n";
-
-    // Please note that << between a and \n is like the concatenation operator.
-    // We can also do this ...
-    cout << "Amar"
-         << "Shonar"
-         << "Bangla" << str << a << "\n ";
+    // TODO: printf(with decimal point clarification), scanf, char/int/doublr array input
     return 0;
 }
