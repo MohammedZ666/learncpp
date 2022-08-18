dbde200c3b162dbcf284af24bb5c0e204dc57bc7
MohammedZ666 <abuobaidazishanegmail.com>
2022-08-18 11:16:15 +0600

    inital commit

--- basic_io.cpp
+++ basic_io.cpp
@@ -1,9 +1,43 @@
-#include <stdio.h>
-
+#include <iostream> // this is for cout, cin
+#include <string>   // this is for string
 using namespace std;
 
 int main()
 {
-    // TODO: printf(with decimal point clarification), scanf, char/int/doublr array input
+    // === input ===
+    // take a integer
+    int a;
+    cout << "Integer dao \n";
+    cin >> a;
+
+    // take a double
+    double b;
+    cout << "Double dao \n";
+    cin >> b;
+
+    // take a string
+    string str;
+    cout << "String dao \n";
+    cin >> str;
+
+    cout << "\n\n\n";
+
+    // == output ==
+    // print int
+    cout
+        << "Integer ->" << a << "\n";
+
+    // pring double
+    cout
+        << "Double ->" << b << "\n";
+
+    // print string
+    cout << "String ->" << str << "\n";
+
+    // Please note that << between a and \n is like the concatenation operator.
+    // We can also do this ...
+    cout << "Amar"
+         << "Shonar"
+         << "Bangla" << str << a << "\n ";
     return 0;
 }
